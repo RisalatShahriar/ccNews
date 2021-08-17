4404dc4d5a06707a8b5bbbc76e613a088a1577bd
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-17 23:53:38 +0600

    Files added

---
+++ Clasissict Express/districtInsights/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DistrictinsightsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'districtInsights'
---
+++ Clasissict Express/diversed/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DiversedConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'diversed'
