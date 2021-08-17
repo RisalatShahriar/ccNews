6f9c7063ea6c64a24758a5909406f207eb4a25c6
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-18 00:00:11 +0600

    Settings done

--- Clasissict Express/ce/settings.py
+++ Clasissict Express/ce/settings.py
@@ -31,6 +31,19 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'achievements',
+    'bdAffirs',
+    'CC',
+    'comics',
+    'culturalInsights',
+    'districtInsights',
+    'diversed',
+    'interviews',
+    'sportsInsights',
+    'trending',
+    'youth',
+    'home',
+    'publish',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
