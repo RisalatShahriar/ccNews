0aab6252e8de86258df96bed5c08b08838dd463d
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-19 22:11:47 +0600

    Registration Backend done

--- Classicist Express/registration/models.py
+++ Classicist Express/registration/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Data(models.Model):
+    accessID = models.CharField(max_length=64)
+    email = models.EmailField(max_length=90)
+    password = models.CharField(max_length=64)
+    number = models.IntegerField(max_length=14)
+    address = models.CharField(max_length=128)
+    post = models.CharField(max_length=20)
--- Classicist Express/registration/views.py
+++ Classicist Express/registration/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from . import models
+
 
 # Create your views here.
 def registration_page(req):
-    return render(req, 'registration.html')
+    if req.method == "POST":
+        if req.POST['password'] == req.POST['password2']:
+            user_cred = User.objects.create_user(username=req.POST['name'], email=req.POST['email'],
+                                            password=req.POST['password'])
+            user_data = models.Data(accessID=req.POST['name'], email=req.POST['email'], password=req.POST['password'],
+                                    number=req.POST['contact'], post=req.POST.get('des'), address=req.POST['address'])
+            user_cred.save()
+            user_data.save()
+            return render(req, 'registration.html')
+    else:
+        return render(req, 'registration.html')
