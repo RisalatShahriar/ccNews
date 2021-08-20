7ecb6cc910df68927e06673b129d0dc532c76bf9
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-20 20:27:26 +0600

    Bugs fixed

--- Classicist Express/registration/views.py
+++ Classicist Express/registration/views.py
@@ -9,8 +9,8 @@ def registration_page(req):
         if req.POST['password'] == req.POST['password2']:
             user_cred = User.objects.create_user(username=req.POST['name'], email=req.POST['email'],
                                             password=req.POST['password'])
-            user_data = models.Data(accessID=req.POST['name'], email=req.POST['email'], password=req.POST['password'],
-                                    number=req.POST['contact'], post=req.POST.get('des'), address=req.POST['address'])
+            user_data = models.Data(accessID=req.POST['name'], email=req.POST['email'], number=req.POST['contact'],
+                                    post=req.POST.get('des'), address=req.POST['address'])
             user_cred.save()
             user_data.save()
             return render(req, 'registration.html')
