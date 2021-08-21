f13212dd15571590f8e5219e34f7a1025c621562
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-21 19:53:48 +0600

    Models added

--- Classicist Express/publish/models.py
+++ Classicist Express/publish/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class News(models.Model):
+    category = models.CharField(max_length=30)
+    heading = models.CharField(max_length=200)
+    news = models.CharField(max_length=1200)
+    tags = models.CharField(max_length=50)
--- Classicist Express/publish/views.py
+++ Classicist Express/publish/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
 
-# Create your views here.
+
 def publish_page(req):
-    return render(req, 'publish.html')
+    if req.method == 'POST':
+        if req.user.is_authenticated:
+            news = models.News(category=req.POST.get('cat'), heading=req.POST['heading'],
+                               news=req.POST['news'], tags=req.POST['tags'])
+            news.save()
+            return render(req, 'publish.html')
+        else:
+            return HttpResponse("Access denied")
+    else:
+        return render(req, 'publish.html')
