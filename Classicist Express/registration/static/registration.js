c34a7ce64b15221e3d55c3059de4ff720812daa2
Srayoshi Ghosh <82891212+Srayo-G@users.noreply.github.com>
2021-08-22 02:31:14 +0600

    image field added

--- Classicist Express/registration/static/registration.js
+++ Classicist Express/registration/static/registration.js
@@ -17,3 +17,16 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+let loader = function (e) {
+  let file = e.target.files;
+
+  let show = "<span>Selected Image: </span>" + file[0].name;
+
+  let output = document.getElementById("selector");
+  output.innerHTML = show;
+  output.classList.add("active");
+};
+
+let fileInput = document.getElementById("file");
+fileInput.addEventListener("change", loader);
