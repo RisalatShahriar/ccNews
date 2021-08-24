060773c69b532c838367da122605b8dcc845528d
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-24 19:54:02 +0600

    User ID fixed

--- Classicist Express/registration/views.py
+++ Classicist Express/registration/views.py
@@ -13,10 +13,8 @@ def registration_page(req):
     if req.user.is_authenticated and check_admin(str(req.user)):
         if req.method == "POST":
             if req.POST['password'] == req.POST['password2']:
-                user_cred = User.objects.create_user(username=req.POST['name'], email=req.POST['email'],
-                                            password=req.POST['password'])
-                user_data = models.Data(accessID=req.POST['name'], email=req.POST['email'], number=req.POST['contact'],
-                                    image=req.FILES.get('image'), post=req.POST.get('des'), address=req.POST['address'])
+                user_cred = User.objects.create_user(username=req.POST['name'], email=req.POST['email'], password=req.POST['password'])
+                user_data = models.Data(accessID=req.POST['name'], email=req.POST['email'], number=req.POST['contact'], image=req.FILES.get('image'), post=req.POST.get('des'), address=req.POST['address'])
                 user_cred.save()
                 user_data.save()
                 return render(req, 'registration.html')
--- Classicist Express/userID/views.py
+++ Classicist Express/userID/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from registration import models
+from django.contrib.auth import logout
 
 
 def userID_home(req, ID):
-    if req.user.is_authenticated and str(req.user) == ID:
-        print(req.user)
+    if req.method == "POST":
+        logout(req)
+        return redirect('login')
+    elif req.user.is_authenticated and str(req.user) == ID:
+        user_info = models.Data.objects.get(accessID=ID)
+        post = user_info.post
+        pic = user_info.image
+        url = f'/user/{ID}'
         return render(req, 'user.html', {
-            "ID": ID
+            "ID": ID,
+            "POST": post,
+            "PROFILE": pic,
+            "acc_url": url
         })
     else:
         return redirect('denied')
