c89dfe2a00bc5d77baf91f391ce72f75339c23f9
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-26 18:09:35 +0600

    Page link --1

--- Classicist Express/home/views.py
+++ Classicist Express/home/views.py
@@ -4,3 +4,37 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req, 'home.html')
+
+def bdaffair_page(req):
+    return render(req, 'home.html')
+
+def cultural_insights_page(req):
+    return render(req, 'home.html')
+
+def sports_insights_page(req):
+    return render(req, 'home.html')
+
+def internatioal_page(req):
+    return render(req, 'home.html')
+
+def interviews_page(req):
+    return render(req, 'home.html')
+
+def cc_page(req):
+    return render(req, 'home.html')
+
+def youth_page(req):
+    return render(req, 'home.html')
+
+def district_insights_page(req):
+    return render(req, 'home.html')
+
+def comics_page(req):
+    return render(req, 'home.html')
+
+def trending_page(req):
+    return render(req, 'home.html')
+
+def diversed_page(req):
+    return render(req, 'home.html')
+
