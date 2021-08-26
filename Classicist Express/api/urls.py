147b574d83be29274a500e35aca14f4f0592dc9c
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-26 17:52:37 +0600

    API added

--- Classicist Express/api/urls.py
+++ Classicist Express/api/urls.py
@@ -2,16 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bd', views.bdaffair, name="bd"),
-    path('home', views.home_data, name='home'),
-    path('cultural', views.cultural_insights, name='cultural'),
-    path('sports', views.sports_insights, name='sports'),
-    path('international', views.internatioal, name='achievements'),
-    path('interview', views.interviews, name='interview'),
-    path('cc', views.cc, name='cc'),
-    path('youth', views.youth, name='youth'),
-    path('district', views.district_insights, name='District'),
-    path('comics', views.comics, name='Comics'),
-    path('trending', views.trending, name='trending'),
-    path('diversed', views.diversed, name='diversed')
+    path('bd', views.bdaffair, name="bd_api"),
+    path('home', views.home_data, name='home_api'),
+    path('cultural', views.cultural_insights, name='cultural_api'),
+    path('sports', views.sports_insights, name='sports_api'),
+    path('international', views.internatioal, name='achievements_api'),
+    path('interview', views.interviews, name='interview_api'),
+    path('cc', views.cc, name='cc_api'),
+    path('youth', views.youth, name='youth_api'),
+    path('district', views.district_insights, name='District_api'),
+    path('comics', views.comics, name='Comics_api'),
+    path('trending', views.trending, name='trending_api'),
+    path('diversed', views.diversed, name='diversed_api')
 ]
--- Classicist Express/home/urls.py
+++ Classicist Express/home/urls.py
@@ -2,5 +2,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('bd', views.bdaffair_page, name="bd"),
+    path('home', views.home_data_page, name='home'),
+    path('cultural', views.cultural_insights_page, name='cultural'),
+    path('sports', views.sports_insights_page, name='sports'),
+    path('international', views.internatioal_page, name='achievements'),
+    path('interview', views.interviews_page, name='interview'),
+    path('cc', views.cc_page, name='cc'),
+    path('youth', views.youth_page, name='youth'),
+    path('district', views.district_insights_page, name='District'),
+    path('comics', views.comics_page, name='Comics'),
+    path('trending', views.trending_page, name='trending'),
+    path('diversed', views.diversed_page, name='diversed')
 ]
