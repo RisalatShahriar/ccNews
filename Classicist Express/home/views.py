708655f0d09b921672c1a2be8dab3ebfbf92c801
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-27 14:45:58 +0600

    Pages added

--- Classicist Express/home/views.py
+++ Classicist Express/home/views.py
@@ -8,35 +8,57 @@ def home(req):
     })
 
 def bdaffair_page(req):
-    return render(req, 'home.html')
+    return render(req, 'bd.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def cultural_insights_page(req):
-    return render(req, 'home.html')
+    return render(req, 'cultural.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def sports_insights_page(req):
-    return render(req, 'home.html')
+    return render(req, 'sports.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def internatioal_page(req):
-    return render(req, 'home.html')
+    return render(req, '.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def interviews_page(req):
-    return render(req, 'home.html')
+    return render(req, 'interview.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def cc_page(req):
-    return render(req, 'home.html')
+    return render(req, 'cc.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def youth_page(req):
-    return render(req, 'home.html')
+    return render(req, 'youth.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def district_insights_page(req):
-    return render(req, 'home.html')
+    return render(req, 'district.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def comics_page(req):
-    return render(req, 'home.html')
+    return render(req, 'comics.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def trending_page(req):
-    return render(req, 'home.html')
+    return render(req, 'trending.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
 def diversed_page(req):
-    return render(req, 'home.html')
+    return render(req, 'diversed.html', {
+        'TITLE': ' | Classicist Express'
+    })
 
