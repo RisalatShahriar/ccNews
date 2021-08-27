764060f9784e1730edb8a8a570d35cbe62002279
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-08-27 22:43:06 +0600

    Base js bug fixed

--- Classicist Express/home/static/base.js
+++ Classicist Express/home/static/base.js
@@ -19,7 +19,7 @@ function topLeft() {
     .then(data => {
         heading.innerHTML = data.heading[1];
         link.href = `/readmore/${data.link[1]}`;
-        news.innerHTML = data.news[1];
+        news.innerHTML = data.front[1];
         picture.src = `/media/${data.picture[1]}`;
     })
 }
