f988693ba4ea9b79ced0a28e4c7d38e52ecbfea4
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-09-01 11:53:35 +0600

    Top API added, bugs yet to solve

--- Classicist Express/api/views.py
+++ Classicist Express/api/views.py
@@ -40,3 +40,39 @@ def trending(req):
 
 def diversed(req):
     return HttpResponse(helper.api_send(models.News.objects.filter(category='Diversed')))
+
+def home_data_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.all().order_by('click').reverse()[:3]))
+
+def bdaffair_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='Bangladesh Affairs').order_by('click').reverse[:3]))
+
+def cultural_insights_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='Cultural Insights').order_by('click').reverse()[:3]))
+
+def sports_insights_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='Sports Insight').order_by('click').reverse()[:3]))
+
+def internatioal_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='International Affairs').order_by('click').reverse()[:3]))
+
+def interviews_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='Small Talk With Gems').order_by('click').reverse()[:3]))
+
+def cc_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='Cultural Classicists').order_by('click').reverse()[:3]))
+
+def youth_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='Youth').order_by('click').reverse()[:3]))
+
+def district_insights_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='District Insights').order_by('click').reverse()[:3]))
+
+def comics_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='Comics').order_by('click').reverse()[:3]))
+
+def trending_top(req):
+    return HttpResponse(helper(models.News.objects.filter(category='Trending').order_by('click').reverse()[:3]))
+
+def diversed_top(req):
+    return HttpResponse(helper.api_send(models.News.objects.filter(category='Diversed').order_by('click').reverse()[:3]))
