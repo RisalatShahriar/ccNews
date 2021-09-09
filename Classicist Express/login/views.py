f8c7a19066cf16a6f241cb0a91238815b1c34725
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-09-10 00:34:32 +0600

    User Id fixed

--- Classicist Express/login/views.py
+++ Classicist Express/login/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponse
 
 
 # Create your views here.
--- Classicist Express/userID/views.py
+++ Classicist Express/userID/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from registration import models
 from django.contrib.auth import logout
+from publish import models as news_models
 
 
 def userID_home(req, ID):
@@ -10,6 +11,7 @@ def userID_home(req, ID):
         return redirect('login')
     elif req.user.is_authenticated and str(req.user) == ID:
         user_info = models.Data.objects.get(accessID=ID)
+        notifications = news_models.News.objects.all()
         post = user_info.post
         pic = user_info.image
         url = f'/user/{ID}'
@@ -17,7 +19,8 @@ def userID_home(req, ID):
             "ID": ID,
             "POST": post,
             "PROFILE": pic,
-            "acc_url": url
+            "acc_url": url,
+            "contents": notifications
         })
     else:
         return redirect('denied')
