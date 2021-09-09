542221b25957ccdcd0edb9d593cb1c1a3be15ceb
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-09-09 23:44:06 +0600

    Login solved

--- Classicist Express/login/views.py
+++ Classicist Express/login/views.py
@@ -4,16 +4,19 @@ from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def login_page(req):
-    if req.method == 'POST':
-        user = authenticate(username=req.POST['id'], password=req.POST['password'])
-        if user is not None:
-            login(req, user)
-            if req.user.is_authenticated:
-                return redirect(f"/user/{req.POST['id']}")
+    if req.user.is_authenticated is not True:
+        if req.method == 'POST':
+            user = authenticate(username=req.POST['id'], password=req.POST['password'])
+            if user is not None:
+                login(req, user)
+                if req.user.is_authenticated:
+                    return redirect(f"/user/{req.POST['id']}")
 
+            else:
+                return render(req, 'login.html', {
+                    "warning": True
+                })
         else:
-            return render(req, 'login.html', {
-                "warning": True
-            })
+            return render(req, 'login.html')
     else:
-        return render(req, 'login.html')
+        return redirect(f"/user/{req.POST['id']}")
