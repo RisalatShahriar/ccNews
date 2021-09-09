7c68b55b34cd2e26d206c83bd5b61dbb279b1f4a
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-09-09 23:32:15 +0600

    Bugs fixed

--- Classicist Express/home/static/base.js
+++ Classicist Express/home/static/base.js
@@ -2,29 +2,132 @@
 const btnHam = document.querySelector(".ham-btn");
 const btnTimes = document.querySelector(".times-btn");
 const navBar = document.getElementById("nav-bar");
+let currentNews = 1;
+
+document.addEventListener('DOMContentLoaded', () => {
+  for (var i = 0; i<5; i++){
+    right()
+  }
+  if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+    for (var i = 0; i<5; i++){
+      right()
+    }
+  }
+})
 
 function topLeft() {
   fetch(`/api/${document.querySelector("#api").value}/top`)
   .then(res => res.json())
-  .then(data => {})
+  .then(data => {
+    if (Object.keys(data.heading).lenght > 1) {
+      const newsDiv = document.querySelector(".container-top-left");
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const div = document.createElement('div');
+      const heading =document.createElement('h3');
+      const news = document.createElement('p');
+      const link = document.createElement('a');
+      newsDiv.append(article);
+      article.append(img);
+      article.append(div);
+      div.append(heading);
+      div.append(news);
+      div.append(link);
+      heading.innerHTML = data.heading[1];
+      link.innerHTML = `Read More <span>>></span>`;
+      link.href = `/readmore/${data.link[1]}`;
+      news.innerHTML = data.front[1];
+      img.src = `/media/${data.picture[1]}`;
+    }
+  })  
 }//Top Stories
 
 function bottomLeft() {
   fetch(`/api/${document.querySelector("#api").value}/top`)
   .then(res => res.json())
-  .then(data => {})
+  .then(data => {
+    for (var i = 0; i<2; i++){
+      const newsDiv = document.querySelector(".container-bottom-left");
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const div = document.createElement('div');
+      const heading =document.createElement('h3');
+      const news = document.createElement('p');
+      const link = document.createElement('a');
+      newsDiv.append(article);
+      article.append(img);
+      article.append(div);
+      div.append(heading);
+      div.append(news);
+      div.append(link);
+      heading.innerHTML = data.heading[i+2];
+      link.innerHTML = `Read More <span>>></span>`;
+      link.href = `/readmore/${data.link[i+2]}`;
+      news.innerHTML = data.front[i+2];
+      img.src = `/media/${data.picture[i+2]}`;
+    }
+  })
 }//Top Stories bottom
 
 function right() {
   fetch(`/api/${document.querySelector("#api").value}`)
   .then(res => res.json())
-  .then(data => {})
+  .then(data => {
+    if (Object.keys(data.heading).length >= currentNews) {
+      const newsDiv = document.querySelector("#right");
+      const article = document.createElement('article');
+      const img = document.createElement('img');
+      const div = document.createElement('div');
+      const heading =document.createElement('h2');
+      const news = document.createElement('p');
+      const link = document.createElement('a');
+      
+      newsDiv.append(article);
+      article.append(div);
+      div.append(heading);
+      div.append(news);
+      div.append(link);
+      article.append(img);
+      
+      heading.innerHTML = data.heading[currentNews];
+      link.innerHTML = `Read More <span>>></span>`;
+      link.href = `/readmore/${data.link[currentNews]}`;
+      news.innerHTML = data.front[currentNews];
+      img.src = `/media/${data.picture[currentNews]}`; 
+      currentNews++;
+    }
+    else {
+      console.log("yo");
+    }
+  })
 }//Latest Stories
 
 function bottom() {
   fetch(`/api/${document.querySelector("#api").value}`)
   .then(res => res.json())
-  .then(data => {})
+  .then(data => {
+    const newsDiv = document.querySelector("#right");
+    const article = document.createElement('article');
+    const img = document.createElement('img');
+    const div = document.createElement('div');
+    const heading =document.createElement('h2');
+    const news = document.createElement('p');
+    const link = document.createElement('a');
+  
+    newsDiv.append(article);
+    article.append(div);
+    div.append(heading);
+    div.append(news);
+    div.append(link);
+    article.append(img);
+  
+    heading.innerHTML = data.heading[currentNews];
+    link.innerHTML = `Read More <span>>></span>`;
+    link.href = `/readmore/${data.link[currentNews]}`;
+    news.innerHTML = data.front[currentNews];
+    img.src = `/media/${data.picture[currentNews]}`;
+    currentNews++
+  })
 }//News
 
 btnHam.addEventListener("click", function () {
