8a65eaaa7af53c926b696bbee0b5e606720d6d8c
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-09-11 21:38:11 +0600

    Title fixed

--- Classicist Express/home/views.py
+++ Classicist Express/home/views.py
@@ -7,84 +7,84 @@ today = date.today().strftime("%B %d, %Y")
 # Create your views here.
 def home(req):
     return render(req, 'home.html', {
-        'TITLE': 'Classicist Express',
+        'TITLE': 'The Classicist Express',
         'api': 'home',
         'date': today
     })
 
 def bdaffair_page(req):
     return render(req, 'bd.html', {
-        'TITLE': 'Bangladesh | Classicist Express',
+        'TITLE': 'Bangladesh | The Classicist Express',
         'api': 'bd',
         'date': today
     })
 
 def cultural_insights_page(req):
     return render(req, 'cultural.html', {
-        'TITLE': 'Cultural | Classicist Express',
+        'TITLE': 'Cultural | The Classicist Express',
         'api': 'cultural',
         'date': today
     })
 
 def sports_insights_page(req):
     return render(req, 'sports.html', {
-        'TITLE': 'Sports | Classicist Express',
+        'TITLE': 'Sports | The Classicist Express',
         'api': 'sports',
         'date': today
     })
 
 def internatioal_page(req):
     return render(req, 'international.html', {
-        'TITLE': 'International | Classicist Express',
+        'TITLE': 'International | The Classicist Express',
         'api': 'international',
         'date': today
     })
 
 def interviews_page(req):
     return render(req, 'interview.html', {
-        'TITLE': 'Interview | Classicist Express',
+        'TITLE': 'Interview | The Classicist Express',
         'api': 'interview',
         'date': today
     })
 
 def cc_page(req):
     return render(req, 'cc.html', {
-        'TITLE': 'Cultural Classicist | Classicist Express',
+        'TITLE': 'Cultural Classicist | The Classicist Express',
         'api': 'cc',
         'date': today
     })
 
 def youth_page(req):
     return render(req, 'youth.html', {
-        'TITLE': 'Youth | Classicist Express',
+        'TITLE': 'Youth | The Classicist Express',
         'api': 'youth',
         'date': today
     })
 
 def district_insights_page(req):
     return render(req, 'district.html', {
-        'TITLE': 'Dictrict | Classicist Express',
+        'TITLE': 'Dictrict | The Classicist Express',
         'api': 'district',
         'date': today
     })
 
 def comics_page(req):
     return render(req, 'comics.html', {
-        'TITLE': 'Comics | Classicist Express',
+        'TITLE': 'Comics | The Classicist Express',
         'api': 'comics',
         'date': today
     })
 
 def trending_page(req):
     return render(req, 'trending.html', {
-        'TITLE': 'Trending | Classicist Express',
+        'TITLE': 'Trending | The Classicist Express',
         'api': 'trending',
         'date': today
     })
 
 def diversed_page(req):
     return render(req, 'diversed.html', {
-        'TITLE': 'Diversed | Classicist Express',
+        'TITLE': 'Diversed | The Classicist Express',
         'api': 'diversed',
         'date': today
     })
