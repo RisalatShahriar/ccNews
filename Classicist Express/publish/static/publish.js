7cbc37d649d65f5a6b6eccb7e6dd9ee82c2d390d
Risalat Shahriar <72489748+RisalatShahriar@users.noreply.github.com>
2021-09-27 00:20:11 +0600

    Bugs fixed

--- Classicist Express/publish/static/publish.js
+++ Classicist Express/publish/static/publish.js
@@ -1,6 +1,20 @@
 var textarea = document.querySelector('#_text');
 
 
+function check(){
+    var file = document.querySelector("#myFile");
+    var count = 1;
+    var type;
+    while (count<4) {
+        op = file.value.lenght-count
+        type += file.value[op]
+        count++
+    }
+    console.log(type)
+    
+    
+}
+
 function bold() {
     textarea.value += '<b></b>';
     textarea.focus();
